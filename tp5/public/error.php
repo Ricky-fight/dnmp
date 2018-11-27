897dfafbaf11db29a73b42e5747e2045e9798031
ricky <18918735979@163.com>
2018-11-27 14:41:45 +0800

    tp5.1+easywechat

---
+++ tp5/public/error.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xingwei
+ * Date: 2018/11/27
+ * Time: 1:25 PM
+ */
+error_reporting(E_ALL);
+ini_set('error_reporting', 'on');
+ini_set('display_errors', 'on');
